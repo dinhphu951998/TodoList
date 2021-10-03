db556942b315e35496940e8a5dee3ababee12253
Nguyen Dinh Phu <dinhphu951998@gmail.com>
2021-10-03 17:56:45 +0700

    update web title

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,7 @@
-import React from 'react';
 import { ItemList } from './components/ItemList';
-
 import './App.css';
-import { ItemListProvider } from './context/TodoListContext'
-import { IItem } from './types/IItem';
-
 
 function App() {
-
-  
 
   return (
     <div className='container'>
